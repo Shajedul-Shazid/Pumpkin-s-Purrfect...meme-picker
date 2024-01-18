d3454dc9f02ccedaee10fc6beaeca822de3e76da
Shajedul-Shazid <programmershajid@gmail.com>
2024-01-18 10:06:42 +0600

    render the radios input

--- index.js
+++ index.js
@@ -18,7 +18,17 @@ function renderEmotionsRadios(cats){
     let radioItems = ``
     const emotions = getEmotionsArray(cats)
     for(let emotion of emotions){
-        radioItems += `<p>${emotion}</p>`
+        radioItems += `
+        <div class="radio">
+            <label for="emotion">${emotion}</label>
+            <input
+            type="radio"
+            id="${emotion}"
+            value="${emotion}"
+            name="emotions"            
+            >
+        </div>
+        `
     }
     emotionRadios.innerHTML = radioItems
     console.log(emotions)
